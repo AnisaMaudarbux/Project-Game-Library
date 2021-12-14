807f84233e4319dbc7cdd72fa5953b364b2466d0
Anisa <anisamaudarbux@gmail.comgit config --global user.email anisamaudarbux@gmail.com>
2021-12-14 13:20:18 +0000

    saving

--- application/models.py
+++ application/models.py
@@ -4,5 +4,5 @@ class Item(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(20))
     publ = db.Column(db.String(100))
-    genre = db.Column(db.String(100))
+    genre = db.Column(db.Text(100))
     hour = db.Column(db.Integer)
