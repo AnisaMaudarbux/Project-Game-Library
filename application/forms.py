71d522b9a9089c20980795a46c42f3e4c40bf302
Anisa <anisamaudarbux@gmail.comgit config --global user.email anisamaudarbux@gmail.com>
2021-12-14 13:07:08 +0000

    editing

--- application/forms.py
+++ application/forms.py
@@ -11,6 +11,6 @@ class AddItem(FlaskForm):
 class EditItem(FlaskForm):
     name = StringField('Game Name', validators=[DataRequired()])
     publ = StringField('Publisher', validators=[DataRequired()])
-    genre = DateField('Genre', format='%Y-%m-%d', validators=[DataRequired()])
-    hours = SelectField('Hours Played', choices=[('todo', 'To-Do'), ('done', 'Done')], validators=[DataRequired()])
+    genre = DateField('Genre', validators=[DataRequired()])
+    hours = SelectField('Hours Played', validators=[DataRequired()])
     submit = SubmitField('Update Item')
