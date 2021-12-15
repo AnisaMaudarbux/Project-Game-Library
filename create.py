a4fc05ea890b528af02e4763925ba42dd3b05748
Anisa <anisamaudarbux@gmail.comgit config --global user.email anisamaudarbux@gmail.com>
2021-12-15 13:11:25 +0000

    working platforms?

--- create.py
+++ create.py
@@ -1,11 +1,11 @@
 from application import db 
-from application.models import  Platform, Game
+from application.models import  Engine, Game
 
 db.drop_all()
 db.create_all()
 
-Rayman = Platform(service = 'PC') #Add example to countries table
-Pokemon_X= Platform(service = '2DS')
+Rayman = Engine(platform = 'PC') #Add example to countries table
+Pokemon_X= Engine(platform = '2DS')
 db.session.add(Rayman)
 db.session.add(Pokemon_X)
 db.session.commit()
