7fd4d4f0dd1c61c45fc565a3ddd4aab413f9edef
Anisa <anisamaudarbux@gmail.comgit config --global user.email anisamaudarbux@gmail.com>
2021-12-15 11:30:26 +0000

    fixing broken

--- application/models.py
+++ application/models.py
@@ -6,4 +6,8 @@ class Game(db.Model):
     dev = db.Column(db.String(100))
     genre = db.Column(db.Text(100))
     hours = db.Column(db.Integer)
+    service = db.Column(db.String, db.ForeignKey('platform.service'))
 
+class Platform(db.Model):
+    service = db.Column(db.String(20), primary_key = True)
+    connection = db.relationship('Game', backref = 'platform')
--- create.py
+++ create.py
@@ -1,4 +1,10 @@
-from application import db
+from application import db 
+from application.models import  Platform
 
-db.drop_all()
 db.create_all()
+
+Rayman = Platform(service = 'PC') #Add example to countries table
+Pokemon_X= Platform(service = '2DS')
+db.session.add(Rayman)
+db.session.add(Pokemon_X)
+db.session.commit()
